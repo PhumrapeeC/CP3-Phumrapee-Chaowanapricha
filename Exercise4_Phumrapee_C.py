89c113790fd23df41e5c684f76197a8e13f49ccb
PhumrapeeChaowanapricha <phuumrapeegenius@gmail.com>
2022-07-20 14:25:00 +0700

    Added Exercise4

---
+++ Exercise4_Phumrapee_C.py
@@ -0,0 +1,10 @@
+foundation_eng_score = 83
+general_business_score = 75
+intro_computer_system_score = 60
+computer_programming_score = 81
+
+print("--- Your Score ---")
+print("Foundation English : ", foundation_eng_score)
+print("General Business : ", general_business_score)
+print("Introduction to Computer Systems  : ", intro_computer_system_score)
+print("Computer Programming : ", computer_programming_score)
