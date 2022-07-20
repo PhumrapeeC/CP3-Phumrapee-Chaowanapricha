2314220f25b7bb7d63b8c70dfbf77c5a13ebfd68
PhumrapeeChaowanapricha <phuumrapeegenius@gmail.com>
2022-07-20 17:07:52 +0700

    Added Exercise5_1

---
+++ Exercise5_1_Phumrapee_C.py
@@ -0,0 +1,7 @@
+num_0 = int(input())
+num_1 = int(input())
+
+print(num_0, " + ", num_1, " = ", num_0 + num_1)
+print(num_0, " - ", num_1, " = ", num_0 - num_1)
+print(num_0, " * ", num_1, " = ", num_0 * num_1)
+print(num_0, " / ", num_1, " = ", num_0 // num_1)
