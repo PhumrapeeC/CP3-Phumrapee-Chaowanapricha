3090e033f1f049ebba4922779103e6c27af49757
PhumrapeeChaowanapricha <phuumrapeegenius@gmail.com>
2022-07-20 14:31:22 +0700

    Fix the type of infomation

--- Exercise4_Phumrapee_C.py
+++ Exercise4_Phumrapee_C.py
@@ -4,7 +4,7 @@ intro_computer_system_score = 60
 computer_programming_score = 81
 
 print("--- Your Score ---")
-print("Foundation English : ", foundation_eng_score)
-print("General Business : ", general_business_score)
-print("Introduction to Computer Systems  : ", intro_computer_system_score)
-print("Computer Programming : ", computer_programming_score)
+print("Foundation English : " + str(foundation_eng_score))
+print("General Business : " + str(general_business_score))
+print("Introduction to Computer Systems  : " + str(intro_computer_system_score))
+print("Computer Programming : " + str(computer_programming_score))
