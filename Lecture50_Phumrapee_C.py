5ec6b1f333866589646b65691fa7bee1a7ecc604
PhumrapeeChaowanapricha <phuumrapeegenius@gmail.com>
2022-07-21 15:54:42 +0700

    Added Lecture50

---
+++ Lecture50_Phumrapee_C.py
@@ -0,0 +1,17 @@
+def addNumber(x, y):
+    print(x + y)
+
+def minusNumber(x, y):
+    print(x - y)
+
+def multiplyNumber(x, y):
+    print(x * y)
+
+def divideNumber(x, y):
+    print(x / y)
+
+
+addNumber(10,5)
+minusNumber(10,5)
+multiplyNumber(10,5)
+divideNumber(10,5)
